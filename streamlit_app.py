13004326d7407916d117d12296ed50f3bb4ac6b0
ddammo55 <ddammo55@naver.com>
2022-08-27 10:13:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import pandas as pd
-
+import plotly.express as px
 view = [100,150,30]
 
 st.write('# Hello World')
+st.
 sview = pd.Series(view)
 sview
 
+df = px.data.iris()
+df["e"] = df["sepal_width"]/100
+fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species", error_x="e", error_y="e")
+fig.show()
