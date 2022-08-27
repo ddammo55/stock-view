a573712cd5e89c4cdb7346a2a481d24e257f4e39
ddammo55 <ddammo55@naver.com>
2022-08-27 10:16:18 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,20 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-view = [100,150,30]
+import plotly.figure_factory as ff
+import numpy as np
 
-st.write('# Hello World')
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
 
-sview = pd.Series(view)
-sview
+# Group data together
+hist_data = [x1, x2, x3]
 
-df = px.data.iris()
-df["e"] = df["sepal_width"]/100
-fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species", error_x="e", error_y="e")
-fig.show()
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+         hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
