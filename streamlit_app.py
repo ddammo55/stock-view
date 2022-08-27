cd8b8bff293e40ee6453a4f74626189efdf651c7
ddammo55 <ddammo55@naver.com>
2022-08-27 15:26:57 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,18 +2,30 @@ import streamlit as st
 import plotly.figure_factory as ff
 import plotly.express as px
 import numpy as np
-import FinanceDataReader as fdr
+
 import pandas as pd
 st.title('# Won Stock')
 
-FAANG = ["FB", "AMZN", "AAPL", "NFLX", "GOOGL"]
-faang_list = [fdr.DataReader(code, '2022', '2023')['Close'] for code in FAANG]
-df_faang = pd.concat(faang_list, axis=1)
-df_faang.columns = FAANG
-df_ratio = df_faang / df_faang.iloc[0] - 1
-df_ratio.columns.name = "company"
-# df = px.line(df_ratio, title="FAANG 일별 수익률")
 
 
 
-st.plotly_chart(df_ratio, use_container_width=True)
+import streamlit as st
+import plotly.figure_factory as ff
+import numpy as np
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+         hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
