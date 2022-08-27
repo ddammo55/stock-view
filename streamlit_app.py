96e233186eb22ff5ada34dcb2114f42ccf09016b
ddammo55 <ddammo55@naver.com>
2022-08-27 12:57:16 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import plotly.figure_factory as ff
 import plotly.express as px
 import numpy as np
 
+st.title('# Won Stock')
+
 df = px.data.stocks()
 df = px.line(df, x="date", y="GOOG", title='구글 차트')
 
